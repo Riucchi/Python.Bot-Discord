880efa1dcc5ca5ab6c5ca50c744739089280bfe5
Lucas <lucasbecca3@hotmail.com>
2024-07-13 18:08:33 -0300

    manejo de multiples canales

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ytdl_options = {
     'format': 'bestaudio/best',
     'yesplaylist': True,  # Enable playlist support
     'noplaylist': None,  # Allow playlist parsing
-    'playlist_items': '1-30',  # Stream items 1 to 10 from the playlist
+    'playlist_items': '1-11',  # Stream items 1 to 10 from the playlist
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
@@ -37,10 +37,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
         super().__init__(source, volume)
 
         self.data = data
-        
-
         self.title = data.get('title')
         self.url = data.get('url')
+        self.entries = []  # Almacenar todas las canciones de la playlist
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=True):
@@ -48,16 +47,17 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
         if 'entries' in data:
-        # Extract all songs from the playlist
-            songs = data['entries']
-            for song in songs:
-                filename = song['url'] if stream else ytdl.prepare_filename(song)
-                player = cls(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=filename, **ffmpeg_options), data=song)
-                client.queue.append(player)
-            return player
+            # Almacenar todas las canciones de la playlist
+            entries = data['entries']
+            for entry in entries:
+                filename = entry['url'] if stream else ytdl.prepare_filename(entry)
+                source = discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=filename, **ffmpeg_options)
+                ytdl_source = cls(source, data=entry)
+                ytdl_source.entries = entries  # Asignar la lista de canciones a cada objeto YTDLSource
+                yield ytdl_source
         else:
             filename = data['url'] if stream else ytdl.prepare_filename(data)
-            return cls(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=filename, **ffmpeg_options), data=data)
+            yield cls(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=filename, **ffmpeg_options), data=data)
     
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -65,6 +65,7 @@ class MyClient(discord.Client):
         self.tree = app_commands.CommandTree(self)
         self.current_voice_channel = None
         self.queue = []
+        self.voice_connections = {}  # Inicializa el diccionario voice_connections
 
     async def setup_hook(self):
         # This copies the global commands over to your guild.
@@ -72,15 +73,20 @@ class MyClient(discord.Client):
         await self.tree.sync(guild=MY_GUILD)
         await self.tree.sync()
 
-    async def play_next(self, interaction, skipped=False):
-        if self.queue:
-            player = self.queue.pop(0)
-            guild = interaction.guild
-            guild.voice_client.play(player, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(interaction, skipped=True), self.loop))
+    async def play_next(self, interaction, voice_channel, skipped=False, message=None):
+        if client.voice_connections[voice_channel]['queue']:
+            player = client.voice_connections[voice_channel]['queue'].pop(0)
+            client.voice_connections[voice_channel]['connection'].play(player, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(interaction, voice_channel, skipped=True), self.loop))
             if not skipped:
-                await interaction.followup.send(f"Playing {player.title}")
+                if message:
+                    await message.edit(content=f"Playing {player.title}")
+                else:
+                    await interaction.followup.send(f"Playing {player.title}")
         else:
-            await interaction.followup.send("No more songs in queue")
+            if message:
+                await message.edit(content="No more songs in queue")
+            else:
+                await interaction.followup.send("No more songs in queue")
 
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
@@ -89,72 +95,79 @@ client = MyClient(intents=intents)
 @client.tree.command()
 async def entrar(interaction: discord.Interaction):
     """ Joins the current voice channel"""
-    if(interaction.user.voice):
+    if interaction.user.voice:
+        voice_channel = interaction.user.voice.channel
+        if voice_channel not in client.voice_connections:
+            client.voice_connections[voice_channel] = {
+                'connection': await voice_channel.connect(),
+                'queue': []
+            }
         await interaction.response.send_message(f"Joining....")
-        client.current_voice_channel = await interaction.user.voice.channel.connect()
     else:
         await interaction.response.send_message("You must be in a voice channel to use this command")
 
 
+@client.tree.command()
 @app_commands.describe(
     url="URL to play"
 )
-@client.tree.command()
 async def reproducir(interaction: discord.Interaction, url: str):
     """ plays a url """
     await interaction.response.defer()
-    player = await YTDLSource.from_url(url, stream=True)
-    client.queue.append(player)
-    if not client.current_voice_channel.is_playing():
-        await client.play_next(interaction, interaction.response)
+    voice_channel = interaction.user.voice.channel
+    if voice_channel in client.voice_connections:
+        async for player in YTDLSource.from_url(url, stream=True):
+            client.voice_connections[voice_channel]['queue'].append(player)
+        if not client.voice_connections[voice_channel]['connection'].is_playing():
+            await client.play_next(interaction, voice_channel)
     await interaction.followup.send(f"Attempting to play {url}")
-    await interaction.followup.send(f"Playing {player.title}")
 
 @client.tree.command()
 async def pause(interaction: discord.Interaction):
     """ Pauses the current audio """
-    if(client.current_voice_channel):
-        if(client.current_voice_channel.is_paused()):
-            await interaction.response.send_message(f"Audio is already paused")
-            return
-        client.current_voice_channel.pause()
+    voice_channel = interaction.user.voice.channel
+    if voice_channel in client.voice_connections:
+        client.voice_connections[voice_channel]['connection'].pause()
         await interaction.response.send_message(f"Audio paused")
     else:
         await interaction.response.send_message("Not currently in a voice channel")
         
+
 @client.tree.command()
 async def resume(interaction: discord.Interaction):
     """ Resumes the current audio """
-    if(client.current_voice_channel):
-        if(client.current_voice_channel.is_paused()):
-            await interaction.response.send_message(f"Resuming audio")
-            client.current_voice_channel.resume()
-        else:
-            await interaction.response.send_message(f"Audio is not currently paused")
+    voice_channel = interaction.user.voice.channel
+    if voice_channel in client.voice_connections:
+        client.voice_connections[voice_channel]['connection'].resume()
+        await interaction.response.send_message(f"Resuming audio")
     else:
         await interaction.response.send_message("Not currently in a voice channel")
 
 @client.tree.command()
 async def stop(interaction: discord.Interaction):
     """ Leaves the current voice channel"""
-    if(client.current_voice_channel):
-        await client.current_voice_channel.disconnect()
+    voice_channel = interaction.user.voice.channel
+    if voice_channel in client.voice_connections:
+        await client.voice_connections[voice_channel]['connection'].disconnect()
+        del client.voice_connections[voice_channel]
         await interaction.response.send_message("Bye bye")
-        client.current_voice_channel = None
     else:
         await interaction.response.send_message("Not currently in a voice channel")
 
 @client.tree.command()
 async def skip(interaction: discord.Interaction):
-    if client.current_voice_channel and client.current_voice_channel.is_playing():
-        client.current_voice_channel.stop()
-        if client.queue:
-            await client.play_next(interaction, skipped=True)
-            await interaction.response.send_message("Skipped to the next song!")
+    """ Skips the current song """
+    voice_channel = interaction.user.voice.channel
+    if voice_channel in client.voice_connections:
+        if client.voice_connections[voice_channel]['connection'].is_playing():
+            client.voice_connections[voice_channel]['connection'].stop()
+            await interaction.response.defer()
+            message = await interaction.followup.send("Skipping song...")
+            await client.play_next(interaction, voice_channel, skipped=True, message=message)
         else:
-            await interaction.response.send_message("No more songs in queue. Stopping music.")
+            await interaction.response.send_message("No hay canción actualmente en reproducción")
     else:
-        await interaction.response.send_message("No hay canción actualmente en reproducción")
+        await interaction.response.send_message("Not currently in a voice channel")
 
 
 client.run(TOKENBOT)
